623ecdbe1c4101d981ae371a1c1fe398e1f499e3
NinnYu Chin <nychin92@gmail.com>
2017-10-25 23:00:35 -0400

    Update ParentChild.java

--- ParentChild.java
+++ ParentChild.java
@@ -6,18 +6,16 @@
  */
 
 import child.Children;
-import parent.Parents;
 
 public class ParentChild {
 	public static void main (String[] args) {
-		Parents dad = new Parents();
 		Children child1 = new Children();
 		
-		System.out.println(	child1.name + "'s father has been arrested " + dad.jailRecord +
-							" times.");
-		child1.hackRecord(dad, (int) (Math.random()*50));
+		System.out.println(	child1.name + "'s father has been arrested " +
+							child1.getRecord() + " times.");
+		child1.hackRecord((int) (Math.random()*50));
 		System.out.println(	"After " + child1.name + " hacked into his father's records,"
-				+ "it now shows that his father has been arrested " + dad.jailRecord + " times.");
-
+							+ "it now shows that his father has been arrested " +
+							child1.getRecord() + " times.");
 	}
 }
