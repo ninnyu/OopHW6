76503ffa80d36422baec7c0ceafa9d098129a832
NinnYu Chin <nychin92@gmail.com>
2017-10-25 22:59:05 -0400

    Update Parents.java

--- parent/Parents.java
+++ parent/Parents.java
@@ -1,10 +1,14 @@
-/* Homework 6, parent package
- * @author NinnYu Chin
- * Description: 	Parent class with a protected member.
+/* 	Homework 6, parent package
+ * 	@author NinnYu Chin
+ * 	Description: 	Parent class with a protected member.
  */
 
 package parent;
 
 public class Parents {
-	public int jailRecord = (int)(Math.random()*50);
+	protected int jailRecord = (int)(Math.random()*50);
+	
+	public int getRecord() {
+		return jailRecord;
+	}
 }
